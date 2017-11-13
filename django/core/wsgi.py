eb12aec502cf9246c89d7642e34d77798c99055c
abhishek <abhishek@magicpin.in>
2017-11-13 16:59:14 +0530

    added signals to stop the loop

--- django/core/wsgi.py
+++ django/core/wsgi.py
@@ -1,33 +1,25 @@
-import logging
-import signal
-from functools import partial
-from os import getpid
-
 import django
-from django.core.handlers.base import get_loop
 from django.core.handlers.wsgi import WSGIHandler
 
-logger = logging.getLogger(__name__)
 
-
-def start_event_loop():
-    loop = get_loop()
-
-    # def _stop(signame):
-    #     logger.warning("got signal %s: exit" % signame)
-    #     loop.stop()
-    #
-    # loop.add_signal_handler(getattr(signal, 'SIGINT'), partial(_stop, 'SIGINT'))
-    # loop.add_signal_handler(getattr(signal, 'SIGTERM'), partial(_stop, 'SIGTERM'))
-
-    logger.info("Event loop running forever, press Ctrl+C to interrupt.")
-    logger.info("pid %s: send SIGINT or SIGTERM to exit." % getpid())
-    try:
-        loop.run_forever()
-    except Exception as e:
-        logging.error(str(e))
-    finally:
-        loop.close()
+# def start_event_loop():
+#     loop = get_loop()
+#
+#     # def _stop(signame):
+#     #     logger.warning("got signal %s: exit" % signame)
+#     #     loop.stop()
+#     #
+#     # loop.add_signal_handler(getattr(signal, 'SIGINT'), partial(_stop, 'SIGINT'))
+#     # loop.add_signal_handler(getattr(signal, 'SIGTERM'), partial(_stop, 'SIGTERM'))
+#
+#     logger.info("Event loop running forever, press Ctrl+C to interrupt.")
+#     logger.info("pid %s: send SIGINT or SIGTERM to exit." % getpid())
+#     try:
+#         loop.run_forever()
+#     except Exception as e:
+#         logging.error(str(e))
+#     finally:
+#         loop.close()
 
 
 def get_wsgi_application():
@@ -39,5 +31,5 @@ def get_wsgi_application():
     case the internal WSGI implementation changes or moves in the future.
     """
     django.setup(set_prefix=False)
-    start_event_loop()
+    #start_event_loop()
     return WSGIHandler()
